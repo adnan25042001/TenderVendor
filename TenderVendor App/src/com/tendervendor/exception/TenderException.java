0e117ea8b2e543a29683153ad83fc224b359efdc
Adnan <adnan.hussain.136660@gmail.com>
2022-11-12 23:46:29 +0530

    addedd userflow

--- TenderVendor App/src/com/tendervendor/exception/TenderException.java
+++ TenderVendor App/src/com/tendervendor/exception/TenderException.java
@@ -9,12 +9,10 @@ public class TenderException extends Exception{
 
 	public TenderException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public TenderException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 }
